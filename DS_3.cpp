da442ab555b0b76d99c33e09aa6485b997bbc7bc
Tony0 <tony0@example.com>
2025-11-04 22:49:31 +0800

    plus T1

--- DS_3.cpp
+++ DS_3.cpp
@@ -1,8 +1,15 @@
+
 #include<iostream>
 #include<string>
 #include <fstream>
 
 using namespace std;
+enum Direction {
+    RIGHT = 0,
+    DOWN = 1,
+    LEFT = 2,
+    UP = 3
+};
 struct Pos {
     int x;
     int y;
@@ -15,8 +22,8 @@ struct StackNode {
 class Maze {
     private:
         char** maze;
-        int x;
-        int y;
+        int x; //長
+        int y; //寬
     public:
         Maze (int x, int y) {
             this -> x = x;
@@ -33,8 +40,24 @@ class Maze {
             delete[] maze;
             maze = nullptr;
         }
-        void SetMaze(char maze_word) {
-            maze[x][y] = maze_word;
+
+        int GetX() {
+            return x;
+        }
+
+        int GetY() {
+            return y;
+        }
+
+        char GetBlock() {
+            return maze[x][y];
+        }
+
+        char GetBlock(int nx, int ny) {
+            return maze[nx][ny];
+        }
+        void SetMaze(int pos_x, int pos_y, char maze_word) {
+            maze[pos_x][pos_y] = maze_word;
         }
 
         void PrintMaze() {
@@ -48,14 +71,14 @@ class Maze {
 
 
 };
-class Stack {
+class RecordMap { // stack
     private:
         StackNode* top;
     public:
-        Stack() {
+        RecordMap() {
             top = nullptr;
         }
-        ~Stack() {
+        ~RecordMap() {
             while (!IsEmpty()) {
                 pop();
             }
@@ -68,8 +91,10 @@ class Stack {
             }
         }
 
-        void push() {
+        void push(int x, int y) {
             StackNode* newnode;
+            newnode -> pos.x = x;
+            newnode -> pos.y = y;
             newnode -> next = top;
             top = newnode;
         }
@@ -82,6 +107,119 @@ class Stack {
         }
 
 
+
+
+
+};
+
+class Mouse {
+    private:
+        Pos pos;
+        Direction dir;
+        Maze in_this_maze;
+        RecordMap visited_route;
+        bool finish = false;
+        bool back = false;
+    public:
+    Mouse(const Maze& maze, const RecordMap& map)
+        : pos{0, 0}, dir(RIGHT), in_this_maze(maze), visited_route(map) {}
+    void ChangeDir() {
+        dir = static_cast<Direction>(dir + 1);
+        if (dir == 4) {
+            dir = RIGHT;
+        }
+    }
+
+    void Walk() {
+        if (dir == RIGHT) {
+            pos.x++;
+        } else if (dir == DOWN) {
+            pos.y++;
+        } else if (dir == LEFT) {
+            pos.x--;
+        } else if (dir == UP) {
+            pos.y--;
+        }
+        visited_route.push(pos.x, pos.y);
+    }
+
+    bool CanWalkTo(int nx, int ny) {
+        if (nx < 0 || ny < 0) {
+            return false;
+        }
+        if (nx >= in_this_maze.GetX() || ny >= in_this_maze.GetY()) {
+            return false;
+        }
+        char block = in_this_maze.GetBlock(nx, ny);
+        if (block == 'O' || block == 'V') {
+            return false;
+        }
+        return true;
+
+    }
+
+    bool Step() {
+    // 試四個方向
+    for (int i = 0; i < 4; ++i) {
+        int nx = pos.x;
+        int ny = pos.y;
+
+        // 依目前 dir 算下一格
+        if (dir == RIGHT)      nx++;
+        else if (dir == DOWN)  ny++;
+        else if (dir == LEFT)  nx--;
+        else if (dir == UP)    ny--;
+
+        // 可以走就走
+        if (CanWalkTo(nx, ny)) {
+            return true;    // 這一步成功
+        }
+
+        // 不行走就換方向再試
+        ChangeDir();
+    }
+
+    // 四個方向都不能走 → 死路 → 回去
+    Back();
+    return false;
+}
+
+    void Back() {
+        in_this_maze.SetMaze(pos.x, pos.y, 'V');
+        if (dir == RIGHT) {
+            pos.x--;
+        } else if (dir == DOWN) {
+            pos.y--;
+        } else if (dir == LEFT) {
+            pos.x++;
+        } else if (dir == UP) {
+            pos.y++;
+        }
+        ChangeDir();
+        visited_route.pop();
+    }
+
+    bool Finish() {
+        if (in_this_maze.GetBlock() == 'G') {
+            return true;
+        }
+        return false;
+    }
+
+    void FindGoal() { //T1
+        bool try_step;
+        visited_route.push(pos.x, pos.y);
+        while (!visited_route.IsEmpty()) {
+            try_step = Step();
+            if (try_step) {
+                Walk();
+            }
+            if (Finish()) {
+                in_this_maze.PrintMaze();
+                break;
+            }
+        }
+    }
 
 
 
