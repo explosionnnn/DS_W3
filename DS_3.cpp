c06181f7214fff5ffe8ce59183a0b2fe66721856
Tony0 <tony0@example.com>
2025-11-06 08:48:01 +0800

    小地方

--- DS_3.cpp
+++ DS_3.cpp
@@ -1,15 +1,17 @@
-
+/*11327121 侯至謙, 11327155 黃宇謙*/
 #include<iostream>
 #include<string>
 #include <fstream>
 
 using namespace std;
+
 enum Direction {
     RIGHT = 0,
     DOWN = 1,
     LEFT = 2,
     UP = 3
 };
+
 struct Pos {
     int x;
     int y;
@@ -19,20 +21,26 @@ struct StackNode {
     Pos pos;
     StackNode* next;
 };
+
 class Maze {
     private:
         char** maze;
         int x; //長
         int y; //寬
     public:
-        Maze (int x, int y) {
-            this -> x = x;
-            this -> y = y;
+        Maze(int x, int y) {
+            this->x = x;
+            this->y = y;
             maze = new char*[x];
             for (int i = 0; i < x; i++) {
                 maze[i] = new char[y];
             }
         }
+
+        ~Maze() {
+            DeleteMaze();
+        }
+
         void DeleteMaze() {
             for (int i = 0; i < x; i++) {
                 delete[] maze[i];
@@ -56,6 +64,7 @@ class Maze {
         char GetBlock(int nx, int ny) {
             return maze[nx][ny];
         }
+
         void SetMaze(int pos_x, int pos_y, char maze_word) {
             maze[pos_x][pos_y] = maze_word;
         }
@@ -63,14 +72,18 @@ class Maze {
         void PrintMaze() {
             for (int i = 0; i < x; i++) {
                 for (int j = 0; j < y; j++) {
-                    cout << maze[x][y];
+                    cout << maze[i][j];
                 }
                 cout << endl;
             }
         }
 
 
+
+
+
 };
+
 class RecordMap { // stack
     private:
         StackNode* top;
@@ -78,21 +91,23 @@ class RecordMap { // stack
         RecordMap() {
             top = nullptr;
         }
+
         ~RecordMap() {
             while (!IsEmpty()) {
                 pop();
             }
         }
+
         void pop() {
             if (!IsEmpty()) {
                 StackNode* del = top;
                 top = top -> next;
-                delete top;
+                delete del;
             }
         }
 
         void push(int x, int y) {
-            StackNode* newnode;
+            StackNode* newnode = new StackNode;
             newnode -> pos.x = x;
             newnode -> pos.y = y;
             newnode -> next = top;
@@ -100,14 +115,15 @@ class RecordMap { // stack
         }
 
         bool IsEmpty() {
-            if (top == nullptr) {
-                return true;
-            }
-            return false;
+            return (top == nullptr);
         }
 
-
-
+        void GetTop(Pos &pos) {
+            if (!IsEmpty()) {
+                pos.x = top -> pos.x;
+                pos.y = top -> pos.y;
+            }
+        }
 
 
 };
@@ -116,13 +132,14 @@ class Mouse {
     private:
         Pos pos;
         Direction dir;
-        Maze in_this_maze;
-        RecordMap visited_route;
+        Maze &in_this_maze;        /*用reference避免Mouse被刪除時*/
+        RecordMap &visited_route;  /*連帶刪除外部Maze和RecordMap指向的物件*/
         bool finish = false;
         bool back = false;
     public:
-    Mouse(const Maze& maze, const RecordMap& map)
+    Mouse(Maze& maze, RecordMap& map)
         : pos{0, 0}, dir(RIGHT), in_this_maze(maze), visited_route(map) {}
+
     void ChangeDir() {
         dir = static_cast<Direction>(dir + 1);
         if (dir == 4) {
@@ -141,6 +158,7 @@ class Mouse {
             pos.y--;
         }
         visited_route.push(pos.x, pos.y);
+        in_this_maze.SetMaze(pos.x, pos.y, 'R');  //走到就設為R
     }
 
     bool CanWalkTo(int nx, int ny) {
@@ -151,7 +169,7 @@ class Mouse {
             return false;
         }
         char block = in_this_maze.GetBlock(nx, ny);
-        if (block == 'O' || block == 'V') {
+        if (block == 'O' || block == 'V' || block == 'R') {  // 這三個都不能走
             return false;
         }
         return true;
@@ -185,25 +203,14 @@ class Mouse {
 }
 
     void Back() {
-        in_this_maze.SetMaze(pos.x, pos.y, 'V');
-        if (dir == RIGHT) {
-            pos.x--;
-        } else if (dir == DOWN) {
-            pos.y--;
-        } else if (dir == LEFT) {
-            pos.x++;
-        } else if (dir == UP) {
-            pos.y++;
-        }
+        in_this_maze.SetMaze(pos.x, pos.y, 'V');  // 退回來變成V
+        visited_route.GetTop(pos);  // 退到堆疊的top
         ChangeDir();
         visited_route.pop();
     }
 
     bool Finish() {
-        if (in_this_maze.GetBlock() == 'G') {
-            return true;
-        }
-        return false;
+        return (in_this_maze.GetBlock(pos.x, pos.y) == 'G');
     }
 
     void FindGoal() { //T1
@@ -213,10 +220,10 @@ class Mouse {
             try_step = Step();
             if (try_step) {
                 Walk();
-            }
-            if (Finish()) {
-                in_this_maze.PrintMaze();
-                break;
+                if (Finish()) {
+                    in_this_maze.PrintMaze();
+                    break;
+                }
             }
         }
     }
@@ -235,11 +242,14 @@ int main() {
     }
     infile >> x;
     infile >> y;
+    if (x <= 0 || y <= 0) {
+        return 1;
+    }
     Maze maze(x, y);
     while (infile >> maze_dot) {
         for (int i = 0; i < x; i++) {
             for (int j = 0; j < y; j++) {
-                maze.SetMaze(maze_dot);
+                maze.SetMaze(i, j, maze_dot);
             }
         }
     }
